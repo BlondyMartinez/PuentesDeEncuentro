87d954dabdc1bbd13f5dd3c28f00d5b8a29558bf
Blondy Martinez <blondymartinezm@gmail.com>
2024-10-26 20:30:39 +0200

    basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css';
+import Navbar from './components/navbar.tsx';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Welcome to My Website!</h1>
+        <p className="mt-2 text-gray-700">This is a simple React and Tailwind CSS setup with TypeScript.</p>
+      </main>
     </>
   )
 }
