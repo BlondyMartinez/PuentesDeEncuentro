f3b9f2f6b87ab649e90b401baae8ac62ec77662d
Blondy Martinez <blondymartinezm@gmail.com>
2024-10-26 20:30:24 +0200

    added i18n for languages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: '#FFFAF2',
+        orange: {
+          DEFAULT: '#F8A340',
+          light: '#FFC38C',
+        },
+        green: {
+          DEFAULT: '#7A9B6C',
+          light: '#A1BE94',
+        },
+        red: {
+          DEFAULT: '#D76258',
+          light: '#FFBCB6',
+        },
+        blue: {
+          DEFAULT: '#9C9CE5',
+          light: '#C2C2F5',
+        },
+      },
+    },
   },
   plugins: [],
 }
