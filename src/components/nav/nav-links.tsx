01c86ef40773695beec38c90ceb3245c7b7ad563
Blondy Martinez <blondymartinezm@gmail.com>
2024-11-07 17:40:40 +0100

    Update nav-links.tsx
    Now dynamically adds / removes links based on the text json and uses react router dom

--- src/components/nav/nav-links.tsx
+++ src/components/nav/nav-links.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 import { useTranslation } from 'react-i18next';
 
 interface Props {
-    isMobile?: boolean;
+  isMobile?: boolean;
 }
 
 const NavLinks: React.FC<Props> = ({ isMobile = false }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const linkClasses = `${isMobile ? 'block text-base' : 'text-sm'} transition ease hover:bg-beige-dark px-3 py-2 rounded-md font-medium duration-300`;
+  const linkClasses = `${isMobile ? 'block text-base' : 'text-sm'} transition ease hover:bg-beige-dark px-3 py-2 rounded-md font-medium duration-300`;
 
-    return (
-        <>
-            <a href="#" className={linkClasses}>{t('navbar.home')}</a>
-            <a href="#" className={linkClasses}>{t('navbar.about')}</a>
-            <a href="#" className={linkClasses}>{t('navbar.services')}</a>
-            <a href="#" className={linkClasses}>{t('navbar.contact')}</a>
-        </>
-    );
+  const navbarLinks = Object.keys(t('navbar', { returnObjects: true })).filter(key => key !== 'language');
+
+  return (
+    <>
+      {navbarLinks.map((linkKey) => (
+        <Link key={linkKey} to={`/${linkKey}`} className={linkClasses}>
+          {t(`navbar.${linkKey}`)}
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default NavLinks;
